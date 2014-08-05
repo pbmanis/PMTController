040da2a0ea40adc949886ecc42c5b8ada6d2df0b
Paul B. Manis <pmanis@med.unc.edu>
2014-08-04 21:29:44 -0400

    Major changes: make PMT data in arrays. Clean up display code, and correct so that it works as intended. Added peak and mean display modes for PMT current. See comments regarding using DACs for voltage control.

--- PMTController.ino
+++ PMTController.ino
@@ -1,29 +1,42 @@
 /*
 PMT Monitor/controller with Arduino Due.
-1. Read and display PMT voltages on analog ports (ADC0, ADC1)
+1. Read and display PMT currents (peak or mean) on analog ports (ADC0, ADC1)
 2. Provide and display command voltages to the PMT on DAC0 and DAC1.
-3. Monitor tube trip status (Hamamatsu H7422P-40 with M9012 controller), and reset PS.
+  -- The DACs on the Due put out 0.55 (1/6 of 3.3V) to about 2.6 (5/6 of 3.3V). This
+  means that the outputs have to be level shifted to reach 0, which requires at least
+  a dual op-amp for the two channels, and a handful of resistors.
+  A simpler solution is to provide manual control of the PMT voltage with a 10-turn 
+  potentiometer, such that the proper voltage range for the PMT is acheived. We then
+  can read the command voltage on additional DACs to report to the host if needed (and,
+  display on the display). In the future, we can provide an option for either manual
+  control or computer control by building the appropriate control circuitry.
+3. Monitor tube trip status (Hamamatsu H7422P-40 with M9012 controller), and provide
+  a signal to reset the PS.
 
-Designed to be used with a basic serial LCD display (16x2) on Serial3
+Designed to be used with a basic serial LCD display (16x2, Hitachi command set)
+connected to TX on Serial3. 
 
 Paul B. Manis, Ph.D.
 UNC Chapel Hill
 This work was supported by NIH/NIDCD grant DC0099809
-7/3/2014 Initial commit
+8/3/2014 Initial commit
+8/4/2014 Major changes to structure, using arrays so that a large number of PMTs could
+(in theory) be controlled. This also simplifies the coding. 
+Added peak/mean measurement of PMT current, and ability to control the time interval over
+which the measurements are made.
 
 This software is free to use and modify under the MIT License (see README.md on github site).
 
 */
+
 #include <stdio.h>
 #include <stdlib.h>
 
-// assign ports
-#define PMT1V 0   // DAC to read
-#define PMT2V 1
-#define PMTCTL1 22  // digital line to monitor for status
-#define PMTCTL2 24
-
-int v1, v2;
+// assign hardware ports
+const unsigned PMT_monitor[] = {0, 1};   // DAC to read
+const unsigned PMT_cmd[]     = {DAC0, DAC1}; //
+const int PMT_ERR_TTL[]      = {22, 24};  // digital line to monitor for status from H7422P-40/M9012
+const int PMT_POW_TTL[]      = {23, 25};  // if using using 7422, digital command to reset
 
 // Definitions for the LCD:
 #define lcdCmd             0xFE   //command prefix
@@ -34,7 +47,7 @@ int v1, v2;
 #define hideCursor         0x0C   //Make cursor invisible
 #define ulCursor           0x0E   //Show underline cursor
 #define blkCursor          0x0D   //Show blinking block cursor
-#define cursorPos          0x80   //set cursor  + position  (row 1=0 to 15, row 2 = 64 to 79)
+#define cursorPos          0x80   //OR with cursor position  (row 1=0 to 15, row 2 = 64 to 79, row3 = 16-31, row4 = 80-95)
 #define scrollRight        0x1C
 #define scrollLeft         0x18
 #define cursorRight        0x14
@@ -44,184 +57,243 @@ int v1, v2;
 #define lcdLine3           0x94
 #define lcdLine4           0xD4
 
-char strPMT1[21];
-char strPMT2[21];
-char strStr1[21];
-char strStr2[21];
+const int lcdBacklight[] = {};
 
-const float vscale = 1000./4096.;  // 3.3V max input = 4096, 4096 = 1000V
-int v1thr = 2.0;  // microamps
-int v2thr = 10.0;
+char str[121];  // general scratch space to build print strings.
 
-int PMT1AnodeMax = 1000;  // anode voltage, V  
-int PMT2AnodeMax = 600;
-float PMT1Anode = 800.;
-float PMT2Anode = 700.;
+/*  Parameters for our specific PMTs and hardware*/
 
-float PMT1min = 0.5, PMT1max = 0.9;   // output voltages to scale
-float PMT2min = 0.5, PMT2max = 1.1;   // min 0, max V is PMTAnode V
-const float dacScale = 4096./3.3;  // AD units per volt.
+float I_PMT[]    = {0., 0.};  // monitor currents from PMTs
+float PMTAnode[] = {0., 0.};  // set to 90% of maximum
+const float GI_PMT[]  = {0.5, 0.1};  // gain of each channel's current preamplifiers, microamperes per volt.
+const float Thr_PMT[] = {1.0, 5.0}; // Threshold for PMT1, microamperes (H7422P-40) and PMT2
+const float PMTmin[]  = {0.5, 0.5};
+const float PMTmax[]  = {0.8, 1.1};   // command voltage range, H7422P-40
+
+float Imeas[]   = {0., 0.};  // measures of current over time (peak or average, in tmeas msec blocks)
+float tmeas     = 200.;
+float nsamp     = 0.0;
+float time_zero = 0.;
+char mode       = 'p';  // single character for data sampling mode: m for mean, p for peak
+
+const float DACscale = 4096/3.3;  // 3.3V max output = 4096, 4096 = 1000V
+const float ADCscale = 3.3/4096.;  // 3.3.V yields 4096 A-D units, so ADCscale * A-D units is V
+const int NPMT = 2;  // number of PMTs supported by this code
 
 void setup() {
-  // put your setup code here, to run once:
+  int i;
+  /* initialize variables */
+  for (i=0; i < NPMT; i++) {
+    PMTAnode[i] = PMTmax[i]*0.9;  // Set to 90% of maximum
+  }
+  
+  // Set up serial port to display, USB port, and hardware configuration
   Serial3.begin(9600); // set up serial port for 9600 baud
   SerialUSB.begin(0);  // back to host computer - speed irrelevant.
-  while(!SerialUSB);  // wait for it to complete
+  while(!SerialUSB);  // wait a USB serial connection to complete
   delay(500); // wait for display to boot up
   Serial3.write(lcdCmd);
   Serial3.write(clearLCD);
   Serial3.write(lcdCmd);
-  Serial3.write(hideCursor);
-  analogReadResolution(12);
+  Serial3.write(hideCursor);  // clear LCD and hide cursor
+  analogReadResolution(12);  // use max resolution on all ADC/DAC
   analogWriteResolution(12);
-  setAnode1(PMT1Anode);
-  setAnode2(PMT2Anode);
-  digitalWrite(PMTCTL1, HIGH); // enable PMT1
-  digitalWrite(PMTCTL2, HIGH); // and 2
-  LCD_NotifyReset(1);
-  LCD_NotifyReset(2);
+  for (i=0; i < NPMT; i++) {
+    setAnode(i, 0.);  // set initial voltages to 0
+    digitalWrite(PMT_POW_TTL[i], HIGH); // enable PMT1
+  }
+  delay(1000.); // give a second to power up
+  for (i = 0; i < NPMT; i++) {
+    setAnode(i, PMTAnode[i]);  // set voltages to 90% of maximum
+    LCD_Notify_Reset(i);
+  }
+  time_zero = millis();
 }
 
 void loop() {
 // 1. Read both ports and check to see if over voltage - if so, shut down pmt
-  int v1, v2;
+  int i;
   char cmd;
-  v1 = analogRead(PMT1V);
-  v2 = analogRead(PMT2V);
-  v1 *= vscale;
-  v2 *= vscale;
-  if (v1 > v1thr) {
-    digitalWrite(PMTCTL1, LOW);
-    LCD_NotifyOver(1);
+  nsamp += 1.0; // count times through loop for mean calculation
+  for (i=0; i<NPMT; i++) {
+    I_PMT[i] = analogRead(PMT_monitor[i]);  // get voltage
+    sprintf(str, "%d = %f at %f", i, I_PMT[i], float(millis()-time_zero));
+    SerialUSB.println(str);
+    I_PMT[i] *= ADCscale*GI_PMT[i]; // convert to voltage, then current in microamps
+    if (I_PMT[i] > Thr_PMT[i]) {
+      digitalWrite(PMT_POW_TTL[i], LOW);  // turn off power to PMT
+      LCD_Notify_Over(i);
+    } 
+    if (mode == 'm') {  // keep running sum and count
+      Imeas[i] = Imeas[i] + I_PMT[i];
+    }
+    if (mode == 'p') { // keep track of peak current
+      if (I_PMT[i] > Imeas[i]) {
+        Imeas[i] = I_PMT[i];
+      }
+    }
   }
-  if (v2 > v2thr) {
-    digitalWrite(PMTCTL2, LOW);
-    LCD_NotifyOver(2);
-  }  
-  LCD_Update(1, (float) v1);
-  LCD_Update(2, (float) v2);
-  delayMicroseconds(5000);
-  //
-  // process incoming commands from computer
+  // check if time to update display
+  if ((millis()-time_zero) >= tmeas) {
+    time_zero = millis();  // reset time count
+    for (i=0; i < NPMT; i++) {
+      if (mode == 'm') {
+        Imeas[i] = Imeas[i]/nsamp; // compute mean
+      }
+      LCD_I_Update(i, Imeas[i]);
+      Imeas[i] = 0.0; // reset values
+    }
+    nsamp = 0.0;
+  }
+  // delayMicroseconds(1000);  // cut sample rate to 1 kHz...
+  
+  // process incoming commands from computer over usb port - if there are any!
   if(SerialUSB.available() > 0) {
     cmd = SerialUSB.read();
     processCmd(cmd);
-  
+  }
 }
 
 void processCmd(char cmd) {
-  int device, value;
-  char strV[21];
+  int device;
+  float interval;
+  float value;
+  char str[81];
   switch(cmd) {
-  case 'v':  //  set a voltage command is: "v1,500\n"
+  case 'v':  //  set a voltage command is: "v1,0.8\n"
     device = SerialUSB.parseInt();  // get the device
-    value = SerialUSB.parseInt();  // get the value
-    if (device == 1) {
-      setAnode1(float(value));
-    }
-    if (device == 2) {
-      setAnode2(float(value));
+    value = SerialUSB.parseFloat();  // get the value
+    if (device >= 0 && device < NPMT) {
+      PMTAnode[device] = value;
+      setAnode(device, value);
     }
     break;
   case 'r':  // read a volage: "r1" for pmt1, etc
-    device=SerialUSB.parseInt()
-    if (device == 1) {
-      value = analogRead(PMT1V)*vscale;
-      SerialUSB.print("V1=")
-      SerialUSB.println(dtostrf((float)vcmd, 5, 1, strV));
+    device=SerialUSB.parseInt();
+    if (device >= 0 && device < NPMT) {
+      value = analogRead(PMT_monitor[device])*ADCscale*GI_PMT[device];
+      sprintf(str, "I%1d=%6.3f uA", device, value);
+      SerialUSB.println(str);
     }
-    if (device == 2) {
-      value = analogRead(PMT2V)*vscale;
-      SerialUSB.print("V2=")
-      SerialUSB.println(dtostrf((float)value, 5, 1, strV));
+    break;
+  case 'R':  // reset a port
+    device = SerialUSB.parseInt();
+    if (device >= 0 && device < NPMT) {
+      resetPMT(device);       
+     }
+     break;
+  case 'a':  //interogate current anode settings
+    device = SerialUSB.parseInt();
+    if (device >= 0 && device < NPMT) {
+       sprintf(str, "Anode%1d=%4.2f V", device, PMTAnode[device]);
+       SerialUSB.println(str);       
     }
+    break;
+  case 'p': //set peak reading mode
+    mode = 'p';
+    LCD_PeakMean(0, mode);
+    LCD_PeakMean(1, mode);
+    break;
+  case 'm': // set mean reading mode
+    mode = 'm';
+    LCD_PeakMean(0, mode);
+    LCD_PeakMean(1, mode);
+    break;
+    
+  case 't': // get a measurement interval (default set above)
+    interval = SerialUSB.parseFloat();
+    if (interval > 10. && interval < 10000.) {  // only accept reasonable values
+      tmeas = interval;
+    }
+    break;
+  case '?': // print a list of commands
+    sprintf(str, "vn,x.y : set the command voltage to the PMT n");
+    SerialUSB.println(str);
+    sprintf(str, "rn      : read the current from the selected PMT n");
+    SerialUSB.println(str);
+    sprintf(str, "Rn      : Reset the power to the selected PMT n");
+    SerialUSB.println(str);
+    sprintf(str, "an      : read the anode voltage setting from the selected PMT n");
+    SerialUSB.println(str);
+    sprintf(str, "p       : select Peak reading mode");
+    SerialUSB.println(str);
+    sprintf(str, "m       : select Mean reading mode");
+    SerialUSB.println(str);
+    sprintf(str, "t###.   : set the reading/averaging period (msec, float)");
+    SerialUSB.println(str);
   }
 }
 
 //*****************************************************************
 // set anode voltages
 
-void setAnode1(float v) {
-  char strV[21];
-  float vf;
-  int vcmd;
-  vf = v/PMT1AnodeMax;  // vf if fraction of voltage range
-  vcmd = PMT1min + vf*PMT1max;  // scale for command 
-  analogWrite(DAC0, int(dacScale*vcmd));
-  SerialUSB.println(dtostrf((float)vcmd, 5, 2, strV));
-  SerialUSB.println(dtostrf((float)(vcmd*dacScale), 5, 0, strV));
-}
-void setAnode2(float v) {
-  float vf;
-  int vcmd;
-  vf = v/PMT2AnodeMax;  // vf if fraction of voltage range
-  vcmd = PMT2min + vf*PMT2max;  // scale for command 
-  analogWrite(DAC1, int(dacScale*vcmd));
+void setAnode(int pmt, float v) {
+  char str[81];
+  float vf, vcmd;
+  PMTAnode[pmt] = v;
+  vf = v/PMTmax[pmt];  // vf if fraction of voltage range
+  vcmd = PMTmin[pmt] + vf*PMTmax[pmt];  // scale for command 
+  analogWrite(PMT_cmd[pmt], int(DACscale*vcmd));
+  LCD_Anode_Update(pmt, v);
 }
 
-float getAnode(int device) {
-  
+// restore power and set the command to the previous value
+void resetPMT(int pmt) {
+    digitalWrite(PMT_POW_TTL[pmt], HIGH); // enable PMT1
+    setAnode(pmt, PMTAnode[pmt]);
+    LCD_Notify_Reset(pmt);
+
 }
 //*****************************************************************
-// Routine to control data display to the LCD
+// Routine to display PMT current levels to the LCD
 // Parameters:
 // 1. Line number (int)
-// 2. parameter (float)
+// 2. parameter (float) or char (for peak/mean mode)
 //
-void LCD_Update(int pmtNo, float value) {
-    char strV[21]; 
-    switch(pmtNo) {
-    case 1: // PMT1
-        Serial3.write(lcdCmd);
-        Serial3.write(lcdLine1);
-        Serial3.print("PMT1 ");
-        dtostrf((float)value, 3, 0, strV);
-        Serial3.print(strV);
-        Serial3.print("V");
-        SerialUSB.println(strV);
-        break;
-    case 2:  // PMT2
-        Serial3.write(lcdCmd);
-        Serial3.write(lcdLine2);
-        Serial3.print("PMT2 ");
-        dtostrf((float)value, 3, 0, strV);
-        Serial3.print(strV);
-        Serial3.print("V");
-        SerialUSB.println(strV);
-        break;
-    }
+const int linecmd[] = {lcdLine1, lcdLine2, lcdLine3, lcdLine4};
+
+const int cpos[] = {0, 64, 16, 80}; // initial numbers to offset cursor by
+const int offset_over = 9;
+const int offset_mode = 8;
+const int offset_anode = 12; // leave space before...
+
+void LCD_I_Update(int pmt, float value) {
+  char strV[21];
+  Serial3.write(lcdCmd);
+  Serial3.write(linecmd[pmt]);
+  sprintf(strV, "%6.3fuA", value);
+  Serial3.print(strV);
 }
 
-void LCD_NotifyOver(int pmtNo) {
-  switch(pmtNo) {
-    case 1:
-      Serial3.write(lcdCmd);
-      Serial3.write(cursorPos);
-      Serial3.write(14);
-      Serial3.print("*");
-      break;  
-    case 2:
-      Serial3.write(lcdCmd);
-      Serial3.write(cursorPos);
-      Serial3.write(78);
-      Serial3.print("*");   
-      break;
-    }
+// indicate whether readings are peak or mean
+void LCD_PeakMean(int pmt, char mode) {
+  Serial3.write(lcdCmd);
+  Serial3.write(cursorPos | (cpos[pmt]+offset_mode));
+  if(mode == 'p') {
+    Serial3.print("p");  // could use symbols instead...
+  }
+  if(mode == 'm') {
+    Serial3.print("m");
+  }
 }
 
-void LCD_NotifyReset(int pmtNo) {
-  switch(pmtNo) {
-    case 1:
-      Serial3.write(lcdCmd);
-      Serial3.write(cursorPos);
-      Serial3.write(14);
-      Serial3.print(" ");  
-      break;
-    case 2:
-      Serial3.write(lcdCmd);
-      Serial3.write(cursorPos);
-      Serial3.write(78);
-      Serial3.print(" ");   
-      break;
-    }
+void LCD_Anode_Update(int pmt, float value) {
+  char strV[21];
+  Serial3.write(lcdCmd);
+  Serial3.write(cursorPos | (cpos[pmt]+offset_anode));
+  sprintf(strV, "%4.2f", value);
+  Serial3.print(strV);
+}
+// notify user of over-current condition
+void LCD_Notify_Over(int pmt) {
+  Serial3.write(lcdCmd);
+  Serial3.write(cursorPos | (cpos[pmt]+offset_over));
+  Serial3.print("*");
+}
+
+// reset the over-current condition notification
+void LCD_Notify_Reset(int pmt) {
+  Serial3.write(lcdCmd);
+  Serial3.write(cursorPos | (cpos[pmt]+offset_over));
+  Serial3.print(" ");  
 }
